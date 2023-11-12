b191ad75aa5a252a3073a549d354aeeacebf796b
johannpa <johann_paque@hotmail.fr>
2023-11-12 15:29:55 +0100

    Created a new folder for the DataContext of the application, the name's forlder is Data

---
+++ EF7Relationships/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF7Relationships.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+
+    }
+}
