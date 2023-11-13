3e61619ef7229472197d90163bbe342f9f4d233f
johannpa <johann_paque@hotmail.fr>
2023-11-13 20:48:08 +0100

    Added connection string and configure it in program.cs

--- EF7Relationships/Program.cs
+++ EF7Relationships/Program.cs
@@ -1,7 +1,11 @@
+using EF7Relationships.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
