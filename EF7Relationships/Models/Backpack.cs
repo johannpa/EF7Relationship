6b487c7fc940d77dbb6a3068ccbc10345d3377ad
johannpa <johann_paque@hotmail.fr>
2023-11-15 21:15:16 +0100

    Added the Backpack model

--- EF7Relationships/Models/Backpack.cs
+++ EF7Relationships/Models/Backpack.cs
@@ -2,5 +2,9 @@
 {
     public class Backpack
     {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public int CharacterId { get; set; }
+        public Character? Character { get; set; }
     }
 }
