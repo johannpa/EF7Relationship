5e47cc13d327720589ef35106086ac7b2f748904
johannpa <johann_paque@hotmail.fr>
2023-11-17 20:43:50 +0100

    Added the DbSet for DataContext

--- EF7Relationships/Data/DataContext.cs
+++ EF7Relationships/Data/DataContext.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EF7Relationships.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EF7Relationships.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-        
 
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Backpack> Backpacks { get; set; }
     }
 }
