e059b54a3f6f7ced14ba4c54f6d2d64e203fa39e
johannpa <johann_paque@hotmail.fr>
2023-11-18 21:49:14 +0100

    Add a list of weapons for the character

--- EF7Relationships/Models/Character.cs
+++ EF7Relationships/Models/Character.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public Backpack? Backpack { get; set; }
+        public List<Weapon>? Weapons { get; set; }
     }
 }
